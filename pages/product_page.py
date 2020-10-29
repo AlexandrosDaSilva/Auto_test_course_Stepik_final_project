4befae60446586d3fd2cc725d4eb73dfc5bad801
Aleksandr Drozdov <a.drozdov@semrush.com>
2020-10-29 18:45:43 +0300

    add negative checks + add test for login link on every page

--- pages/product_page.py
+++ pages/product_page.py
@@ -15,3 +15,9 @@ class ProductPage(BasePage):
         item_price = self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text
         message_price = self.browser.find_element(*ProductPageLocators.MESSAGE_ITEM_PRICE).text
         assert item_price == message_price, 'Item PRICE does not match with the one you added'
+
+    def should_disappear_success_message_about_adding_to_cart(self):
+        assert self.is_disappeared(*ProductPageLocators.MESSAGE_ITEM_NAME), "Success message did not disappear"
+
+    def should_not_be_success_message_about_adding_to_cart(self):
+        assert self.is_not_element_present(*ProductPageLocators.MESSAGE_ITEM_NAME), "Success message is presented"
