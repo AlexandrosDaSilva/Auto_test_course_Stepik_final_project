9dac92c2bfcc1efbd547ecbdf14aade65c5a7f36
Aleksandr Drozdov <a.drozdov@semrush.com>
2020-10-29 15:26:42 +0300

    add ProductPage and add_to_basket test

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+class ProductPageLocators():
+    ADD_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    ITEM_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    ITEM_PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
+    MESSAGE_ITEM_NAME = (By.CSS_SELECTOR, '#messages .alert:nth-child(1) strong')
+    MESSAGE_ITEM_PRICE = (By.CSS_SELECTOR, '#messages .alert:nth-child(3) strong')
---
+++ pages/product_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
+        add_button.click()
+
+    def should_be_correct_item_name(self):
+        item_name = self.browser.find_element(*ProductPageLocators.ITEM_NAME).text
+        message_name = self.browser.find_element(*ProductPageLocators.MESSAGE_ITEM_NAME).text
+        assert item_name == message_name, 'Item NAME does not match with the one you added'
+
+    def should_be_correct_item_price(self):
+        item_price = self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text
+        message_price = self.browser.find_element(*ProductPageLocators.MESSAGE_ITEM_PRICE).text
+        assert item_price == message_price, 'Item PRICE does not match with the one you added'
