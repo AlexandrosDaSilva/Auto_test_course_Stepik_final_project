69dcb088d89f4aa10ae2c6450955f006bd1e596c
Aleksandr Drozdov <a.drozdov@semrush.com>
2020-10-30 11:48:07 +0300

    add basketPage and tests for it

--- pages/locators.py
+++ pages/locators.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".btn-group a.btn.btn-default")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
@@ -17,3 +18,7 @@ class ProductPageLocators():
     ITEM_PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
     MESSAGE_ITEM_NAME = (By.CSS_SELECTOR, '#messages .alert:nth-child(1) strong')
     MESSAGE_ITEM_PRICE = (By.CSS_SELECTOR, '#messages .alert:nth-child(3) strong')
+
+class BasketPageLocators():
+    BASKET_ITEM = (By.CSS_SELECTOR, '.basket_summary')
+    BASKET_EMPTY_MESSAGE = (By.CSS_SELECTOR, '#content_inner > p')
--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 import pytest
 
 # @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -19,30 +20,30 @@ import pytest
 #     page.solve_quiz_and_get_code()
 #     page.should_be_correct_item_name()
 #     page.should_be_correct_item_price()
-# 
-# @pytest.mark.xfail
-# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-#     link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-#     page = ProductPage(browser, link)  # initialize Page Object, giving browser and url as parameters to constructor
-#     page.open()
-#     page.add_to_basket()
-#     page.solve_quiz_and_get_code()
-#     page.should_not_be_success_message_about_adding_to_cart()
-#
-# def test_guest_cant_see_success_message(browser):
-#     link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-#     page = ProductPage(browser, link)  # initialize Page Object, giving browser and url as parameters to constructor
-#     page.open()
-#     page.should_not_be_success_message_about_adding_to_cart()
-#
-# @pytest.mark.xfail
-# def test_message_disappeared_after_adding_product_to_basket(browser):
-#     link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-#     page = ProductPage(browser, link)  # initialize Page Object, giving browser and url as parameters to constructor
-#     page.open()
-#     page.add_to_basket()
-#     page.solve_quiz_and_get_code()
-#     page.should_disappear_success_message_about_adding_to_cart()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    page = ProductPage(browser, link)  # initialize Page Object, giving browser and url as parameters to constructor
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message_about_adding_to_cart()
+
+def test_guest_cant_see_success_message(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    page = ProductPage(browser, link)  # initialize Page Object, giving browser and url as parameters to constructor
+    page.open()
+    page.should_not_be_success_message_about_adding_to_cart()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    page = ProductPage(browser, link)  # initialize Page Object, giving browser and url as parameters to constructor
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_disappear_success_message_about_adding_to_cart()
 
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -55,3 +56,12 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)  # initialize Page Object, giving browser and url as parameters to constructor
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_empty_basket_message()
